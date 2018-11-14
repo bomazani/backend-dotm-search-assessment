54314fa5fe348ba147893d08c38284536d80136f
Bob Hunt <bob.hunt.email@gmail.com>
2018-11-14 16:59:59 -0500

    Able to locate '$' and return string of 40 characters before & after.

--- dotm_search.py
+++ dotm_search.py
@@ -23,7 +23,12 @@ def find_dotm(path_to_search, text_to_find):
     # create counters
     file_count = 0
     search_count = 0
+    dotm_count = 0
+    xml_count = 0
     match_count = 0
+    found_count = 0
+    open_count = 0
+    char_count = 0
     # get a list of files to iterate over. maybe use os.listdir?
     dirs = os.listdir(path_to_search)
     # be sure to exclude any files not ending with ".dotm"
@@ -32,35 +37,68 @@ def find_dotm(path_to_search, text_to_find):
         get_ext = os.path.splitext(file)
         if get_ext[1] == '.dotm':
             # print(path_to_search)
-            # print(file)
-            search_count += 1
+            dotm_count += 1
             fill_path = os.path.join(path_to_search, file)
-            print(fill_path)
+            # print(fill_path)
             # open zip files like this:
             # with zipfile.ZipFile(fill_path) as z:
             with zipfile.ZipFile(fill_path, "r") as z:
-                print('z = ' + str(z))
+                # print('***************')
+                # Get z's table of contents:
                 names = z.namelist()
-                print(names)
+                found_files = list(z.namelist())
+                for file in found_files:
+                    found_count += 1
+                    # print('found file = ' + str(file))
+                xml_files = list(
+                    filter(lambda x: x.endswith('.xml'), z.namelist()))
+                # print(names)
+                print('---------------')
+                # print(xml_files)
+                for file in xml_files:
+                    xml_count += 1
+                    # try:
+                    with z.open(file, "r") as xml:
+                        content = list(xml.read())
+                        open_count += 1
+                            # print('**** Next File ****')
+                            # print(content)
+                            # char_count = 0
+                            # i = 0
+                            # while i < len(content):
+                            #     if content[i] == '$':
+                            #         start = i - 40
+                            #         if start < 0:
+                            #             start = start + 40
+                            #         end = i + 40
+                            #         print(content[start:end])
+                            #         i +=i
+                        for index, search_item in enumerate(content):
+                            seperator = ''
+                            if search_item == '$':
+                                print('Search_Item = ' + search_item)
+                                print('index = ' + str(index))
+                                found_item = content[index-40:index+40]
+                                found_string = ''
+                                for i in found_item:
+                                    found_string += i
+                                print('found_string = ' + str(found_string))
 
-                # with open(z, 'r') as zf:
-                #     names = zf.namelist()
-                #     print(names)
-                # for item in z:
-                #     match_count += 1
-    print(file_count)
-    print(search_count)
-    print(match_count)
-    
-  
-    # Get z's table of contents:
+
     # names = z.namelist()
-   
+
     # using z again, open the 'word/document.xml' file
     # read the z file line by line, and search for text_to_search
     # if you find a line containing the desired text, print it to console
-    # update your match_count and search_count appropriately 
-  
+    # update your match_count and search_count appropriately
+    print('file_count = ' + str(file_count))
+    print('search_count = ' + str(search_count))
+    print('found_count = ' + str(found_count))
+    print('match_count = ' + str(match_count))
+    print('dotm_count = ' + str(dotm_count))
+    # print('word_doc_count = ' + str(word_doc_count))
+    print('xml_count = ' + str(xml_count))
+    print('open_count = ' + str(open_count))
 
 
 def create_parser():
@@ -68,7 +106,8 @@ def create_parser():
     parser = argparse.ArgumentParser()
     # add my own arguments that I expect on the cmd line
     parser.add_argument('text', help='text to search for dotm files')
-    parser.add_argument('-d','--dir', help='directory of dotm files to search', default='.')
+    parser.add_argument(
+        '-d', '--dir', help='directory of dotm files to search', default='.')
     return parser
 
 
@@ -80,16 +119,16 @@ def main():
     if not my_args:
         parser.print_usage()
         sys.exit(1)
-        
 
     text_to_find = my_args.text
     path_to_search = my_args.dir
-    find_dotm(path_to_search,text_to_find)
+    find_dotm(path_to_search, text_to_find)
 
     # print(my_args.dir)
     # print(text_to_find)
     # print(my_args.text)
     # print(path_to_search)
+
 
 if __name__ == '__main__':
     main()
